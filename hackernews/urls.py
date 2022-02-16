d44a02d34f7e648e8fded144a1cdcae2f0919644
briantical <lutbrianivan@gmail.com>
2022-02-16 22:38:44 +0300

    implemented queries

--- hackernews/urls.py
+++ hackernews/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
